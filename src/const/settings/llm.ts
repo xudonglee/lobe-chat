43db8cc8e9aa871511651911282e3e109667ca83
xudonglee <xudongleee@126.com>
2024-12-29 11:31:39 +0800

    Bugfix: Error: Expected object keys to be in ascending order. 'anthropic' should be before 'openai'.

--- src/const/settings/llm.ts
+++ src/const/settings/llm.ts
@@ -2,13 +2,6 @@ import { ModelProvider } from '@/libs/agent-runtime';
 import { genUserLLMConfig } from '@/utils/genUserLLMConfig';
 
 export const DEFAULT_LLM_CONFIG = genUserLLMConfig({
-  ollama: {
-    enabled: false, // This will hide the ollama in the chat list default
-    fetchOnClient: true,
-  },
-  openai: {
-    enabled: true,
-  },
 
   // Display this two default
   anthropic: {
@@ -17,7 +10,14 @@ export const DEFAULT_LLM_CONFIG = genUserLLMConfig({
   deepseek: {
     enabled: true,
   },
-
+  
+  ollama: {
+    enabled: false, // This will hide the ollama in the chat list default
+    fetchOnClient: true,
+  },
+  openai: {
+    enabled: true,
+  },
 });
 
 export const DEFAULT_MODEL = 'gpt-4o-mini';
